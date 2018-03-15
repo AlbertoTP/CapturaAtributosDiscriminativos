94bfe2ceb45031f7399ec10fa00aeeaa3986c11c
AlbertoTP <alberto.tapia.p@gmail.com>
2018-03-15 00:42:52 -0600

    Ejemplos funcionando

--- examplesPythonLibraries/spaCy.py
+++ examplesPythonLibraries/spaCy.py
@@ -5,8 +5,42 @@ Created on Sun Feb  4 19:51:51 2018
 @author: alternatif
 """
 
+import spacy
+
+nlp = spacy.load('en')
+#nlp = spacy.load('en_core_web_sm')
+
+
 from spacy.tokens import Token
 fruit_getter = lambda token: token.text in ('apple', 'pear', 'banana')
 Token.set_extension('is_fruit', getter=fruit_getter)
 doc = nlp(u'I have an apple')
 assert doc[3]._.is_fruit
+
+
+tokens = nlp(u'dog cat banana')
+
+for token1 in tokens:
+    for token2 in tokens:
+        print(token1, token2, token1.similarity(token2))
+        
+        
+#nlp = spacy.load('en_core_web_lg')
+#tokens = nlp(u'dog cat banana sasquatch')
+#
+#for token in tokens:
+#    print(token.text, token.has_vector, token.vector_norm, token.is_oov)
+    
+doc = nlp(u'Apple is looking at buying U.K. startup for $1 billion')
+
+for ent in doc.ents:
+    print(ent.text, ent.start_char, ent.end_char, ent.label_)
+
+
+
+import spacy
+from spacy import displacy
+
+nlp = spacy.load('en')
+doc = nlp(u'This is a sentence.')
+displacy.serve(doc, style='dep')
--- examplesPythonLibraries/textblod.py
+++ examplesPythonLibraries/textblod.py
@@ -8,12 +8,16 @@ or the get_synsets method, optionally passing in a part of speech.
 
 """
 
+
+import spacy
+nlp = spacy.load('en')
+
 from textblob import Word
 from textblob.wordnet import VERB
 word = Word("octopus")
-print word.synsets
+print (word.synsets)
 #[Synset('octopus.n.01'), Synset('octopus.n.02')]
-print Word("hack").get_synsets(pos=VERB)
+print (Word("hack").get_synsets(pos=VERB))
 #[Synset('chop.v.05'), Synset('hack.v.02'), Synset('hack.v.03'), Synset('hack.v.04'), Synset('hack.v.05'), Synset('hack.v.06'), Synset('hack.v.07'), Synset('hack.v.08')]
 
 """
@@ -21,10 +25,10 @@ You can access the definitions for each synset via the definitions property
 or the define() method, which can also take an optional part-of-speech argument.
 """
 
-print Word("octopus").definitions
+print (Word("octopus").definitions)
 #['tentacles of octopus prepared as food', 'bottom-living cephalopod having a soft oval body with eight long tentacles']
-print ">Ejemplo banana"
-print Word("banana").definitions
+print (">Ejemplo banana")
+print (Word("banana").definitions)
 
 
 """
@@ -33,31 +37,31 @@ You can also create synsets directly.
 from textblob.wordnet import Synset
 octopus = Synset('octopus.n.02')
 shrimp = Synset('shrimp.n.03')
-print "Synsets octopus - shrimp = ",octopus.path_similarity(shrimp)
+print ("Synsets octopus - shrimp = ",octopus.path_similarity(shrimp))
 #0.1111111111111111
 
 apple = Synset('apple.n.01')
 banana = Synset('banana.n.01')
-print "Synsets apple - banana = ",apple.path_similarity(banana)
+print ("Synsets apple - banana = ",apple.path_similarity(banana))
 
 
-print ">Ejemplo apple"
+print (">Ejemplo apple")
 significados=Word("apple").synsets
-print significados
+print (significados)
 for i in significados:
-    print i.definition()
+    print (i.definition())
 
 
 """
 You can restrict which synsets to retrieve for a word for that word only
 when used as a particular part of speech (say noun or verb).
 """
-print "-----------------"
+print ("-----------------")
 from textblob import Word
 from textblob.wordnet import NOUN
 synsets = Word("apple").get_synsets(pos=NOUN)
 for synset in synsets:
-    print synset.definition()
+    print (synset.definition())
     
 """
 We can also take any synset and use its .lemma_names()
@@ -66,13 +70,13 @@ essentially giving us a list of synonyms (words that mean the same thing).
 Let’s find synonyms for “bank” in the sense of “financial institutions”
 (element 1 from the list above):
 """
-print "-----------------"
+print ("-----------------")
 from textblob import Word
 from textblob.wordnet import NOUN
 synsets = Word("apple").get_synsets(pos=NOUN)
-print ">solo uno"
-print synsets[1].lemma_names()
-print ">todos"
+print (">solo uno")
+print (synsets[1].lemma_names())
+print (">todos")
 for synset in synsets:
-    print synset.lemma_names()
+    print (synset.lemma_names())
 #[u'depository_financial_institution', u'bank', u'banking_concern', u'banking_company']
