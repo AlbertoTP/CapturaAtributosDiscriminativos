042eb33ced2dd4692244279b5ac257f628bad120
AlbertoTP <alberto.tapia.p@gmail.com>
2018-03-23 16:00:54 -0600

    Agrego condición de agregar a diccionario negativo, revisar

--- Model/extractTrain.py
+++ Model/extractTrain.py
@@ -27,25 +27,28 @@ def readFile(ruta,diccPos,diccNeg):
     - label ("1" if the feature characterizes word 1 compared to word 2, "0" otherwise)
     Example:    word1,word2,feature,label(0/1)
     """
+    cont=0
     for linea in file.readlines():
         line=linea.split(",")
         #print line
         if line[3]=='1\n':          #agrega a positivos
-            #for 3 keys 2 values
-            #diccPos[line[0],line[1],line[2]]=[line[0],line[2]]
-            #for 2 keys 1 value
             if diccPos.has_key((line[0],line[2])):
                 diccPos[line[0],line[2]].append(line[1])
             else:
                 diccPos[line[0],line[2]]=[line[1]]
+                if diccNeg.has_key((line[1],line[2])):
+                    diccNeg[line[1],line[2]].append(line[0])
+                else:
+                    diccNeg[line[1],line[2]]=[line[0]]
         else:            #agrega a negativos
-            #for 3 keys 2 values
-            #diccNeg[line[0],line[1],line[2]]=[line[0],line[2]]
-            #for 2 keys 1 value
-            if diccNeg.has_key((line[0],line[2])):
-                diccNeg[line[0],line[2]].append(line[1])
+            if not(diccPos.has_key((line[0],line[2]))):
+                if diccNeg.has_key((line[0],line[2])):
+                    diccNeg[line[0],line[2]].append(line[1])
+                else:
+                    diccNeg[line[0],line[2]]=[line[1]]
             else:
-                diccNeg[line[0],line[2]]=[line[1]]
+                cont+=1
+    print "descarto ",cont,"palabras duplicadas en diccionario"
     file.close()
     return diccPos,diccNeg
     
@@ -53,7 +56,8 @@ def main():
     starting_point = time.time()
     dicPos={}
     dicNeg={}
-    dicPos,dicNeg=readFile("dataTrain/train.txt",dicPos,dicNeg) #diccionarios del train
+    #dicPos,dicNeg=readFile("dataTrain/train.txt",dicPos,dicNeg) #diccionarios del train
+    dicPos,dicNeg=readFile("dataTrain/validation.txt",dicPos,dicNeg)
     #for key in dicPos: #imprime todo el diccionario
         #for 3 key 2 values
         #print "Word1: "+key[0]+"\tWord2: "+key[1]+"\tAtrib: "+key[2]+"\nW1|A: "+dicPos[key][0]+"|"+dicPos[key][1]
@@ -61,6 +65,12 @@ def main():
         #print "Word1: "+key[0]+"\tWord2: "+dicPos[key][0]+"\tAtrib: "+key[1]+"\nA: "+str(dicPos[key])
     print "elementos dicPos ",len(dicPos)
     print "elementos dicNeg ",len(dicNeg)
+    
+    print dicPos.has_key(('elbow', 'arm'))
+    print dicNeg.has_key(('elbow', 'arm'))
+    
+    print dicPos.has_key(('barn', 'large'))
+    print dicNeg.has_key(('barn', 'large'))
     #print dicPos.keys() #llaves
     #print dicPos.values() #valor de llaves
     #print dicPos.get(('rabbit', 'hops'))
