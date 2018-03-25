17f8ad3de835dd86dd8524d68861710dd108f97e
AlbertoTP <alberto.tapia.p@gmail.com>
2018-03-24 20:13:57 -0600

    Modifico forma de extraer la informacion de archivos train

--- Model/extractTrain.py
+++ Model/extractTrain.py
@@ -6,6 +6,16 @@
 import sys
 import time
 
+
+def compareDic(dicPos,dicNeg):
+    for key in dicPos:
+        #print "key ",key,type(key)
+        if dicNeg.has_key(key):
+            del dicNeg[key]
+            #print "delete ",key
+    return dicNeg
+
+
 def readFile(ruta,diccPos,diccNeg):
     """
     Read file, extract information (Word 1, word 2, Feature, value) line per line, insert in dictionaries
@@ -27,50 +37,51 @@ def readFile(ruta,diccPos,diccNeg):
     - label ("1" if the feature characterizes word 1 compared to word 2, "0" otherwise)
     Example:    word1,word2,feature,label(0/1)
     """
-    cont=0
+    linePos,lineNeg=0,0
     for linea in file.readlines():
         line=linea.split(",")
         #print line
         if line[3]=='1\n':          #agrega a positivos
+            linePos+=1
             if diccPos.has_key((line[0],line[2])):
                 diccPos[line[0],line[2]].append(line[1])
             else:
                 diccPos[line[0],line[2]]=[line[1]]
-                if diccNeg.has_key((line[1],line[2])):
-                    diccNeg[line[1],line[2]].append(line[0])
-                else:
-                    diccNeg[line[1],line[2]]=[line[0]]
+            if diccNeg.has_key((line[1],line[2])):
+                diccNeg[line[1],line[2]].append(line[0])
+            else:
+                diccNeg[line[1],line[2]]=[line[0]]
         else:            #agrega a negativos
+            lineNeg+=1
             if not(diccPos.has_key((line[0],line[2]))):
                 if diccNeg.has_key((line[0],line[2])):
                     diccNeg[line[0],line[2]].append(line[1])
                 else:
                     diccNeg[line[0],line[2]]=[line[1]]
             else:
-                cont+=1
-    print "descarto ",cont,"palabras duplicadas en diccionario"
+                if not(diccPos.has_key((line[1],line[2]))):
+                    diccPos[line[1],line[2]]=[line[0]]
+    diccNeg=compareDic(diccPos,diccNeg)
+    print "Archivo: ",ruta
+    print "lineas Pos: ",linePos,"\nlineas Neg: ",lineNeg,"\n"
     file.close()
     return diccPos,diccNeg
+    
     
 def main():
     starting_point = time.time()
     dicPos={}
     dicNeg={}
     #dicPos,dicNeg=readFile("dataTrain/train.txt",dicPos,dicNeg) #diccionarios del train
-    dicPos,dicNeg=readFile("dataTrain/validation.txt",dicPos,dicNeg)
+    dicPos,dicNeg=readFile("dataTrain/validation.txt",dicPos,dicNeg)    
     #for key in dicPos: #imprime todo el diccionario
-        #for 3 key 2 values
-        #print "Word1: "+key[0]+"\tWord2: "+key[1]+"\tAtrib: "+key[2]+"\nW1|A: "+dicPos[key][0]+"|"+dicPos[key][1]
-        #for 2 key 1 value
         #print "Word1: "+key[0]+"\tWord2: "+dicPos[key][0]+"\tAtrib: "+key[1]+"\nA: "+str(dicPos[key])
     print "elementos dicPos ",len(dicPos)
     print "elementos dicNeg ",len(dicNeg)
     
-    print dicPos.has_key(('elbow', 'arm'))
-    print dicNeg.has_key(('elbow', 'arm'))
+    #print dicPos.has_key(('elbow', 'arm'))
+    #print dicNeg.has_key(('elbow', 'arm'))
     
-    print dicPos.has_key(('barn', 'large'))
-    print dicNeg.has_key(('barn', 'large'))
     #print dicPos.keys() #llaves
     #print dicPos.values() #valor de llaves
     #print dicPos.get(('rabbit', 'hops'))
