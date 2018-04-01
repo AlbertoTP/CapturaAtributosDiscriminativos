e8a1820db24bca8a425087120b7fc3a410da703f
AlbertoTP <alberto.tapia.p@gmail.com>
2018-03-31 20:08:34 -0600

    Add coments and other configuration to extract and insert information

--- Model/extractTrain.py
+++ Model/extractTrain.py
@@ -8,63 +8,74 @@ import time
 
 
 def compareDic(dicPos,dicNeg):
+    """
+    Delete duplicate keys in dictionay negative
+    Input: 2 dictionaries dicPos and dicNeg (dictionary)
+    Return: negative dictionary without duplicate keys (dictionary)
+    """
+    #cont=0
     for key in dicPos:
         #print "key ",key,type(key)
         if dicNeg.has_key(key):
             del dicNeg[key]
             #print "delete ",key
+            #cont+=1
+    #print "Se descartaron ",cont," palabras"
     return dicNeg
 
 
 def readFile(ruta,diccPos,diccNeg):
     """
     Read file, extract information (Word 1, word 2, Feature, value) line per line, insert in dictionaries
-    Input: ruta path of the file to be read
-            diccPos dictionary of candidate positive examples
-            diccNeg dictionary of candidate negative examples
-    Return: 2 dictionaries diccPos and diccNeg
+    Input: ruta path of the file to be read (String)
+            diccPos dictionary of candidate positive examples (dictionary)
+            diccNeg dictionary of candidate negative examples (dictionary)
+    Return: 2 dictionaries diccPos and diccNeg (dictionary)
     """
-    try:
-        file = open(ruta,"r")
-    except IOError:
-        print "There was an ERROR reading file"
-        sys.exit()
-    """
-    Data format: 4 comma-separated fields:
-    - word 1 (pivot)
-    - word 2 (comparison)
-    - feature
-    - label ("1" if the feature characterizes word 1 compared to word 2, "0" otherwise)
-    Example:    word1,word2,feature,label(0/1)
-    """
-    linePos,lineNeg=0,0
-    for linea in file.readlines():
-        line=linea.split(",")
-        #print line
-        if line[3]=='1\n':          #agrega a positivos
-            linePos+=1
-            if diccPos.has_key((line[0],line[2])):
-                diccPos[line[0],line[2]].append(line[1])
-            else:
-                diccPos[line[0],line[2]]=[line[1]]
-            if diccNeg.has_key((line[1],line[2])):
-                diccNeg[line[1],line[2]].append(line[0])
-            else:
-                diccNeg[line[1],line[2]]=[line[0]]
-        else:            #agrega a negativos
-            lineNeg+=1
-            if not(diccPos.has_key((line[0],line[2]))):
-                if diccNeg.has_key((line[0],line[2])):
-                    diccNeg[line[0],line[2]].append(line[1])
+    print "\n>>> Archivo: ",ruta," extrayendo datos ..."
+    for i in range (0,2):
+        #print ">>>",i
+        try:
+            file = open(ruta,"r")
+        except IOError:
+            print "There was an ERROR reading file"
+            sys.exit()
+        """
+        Data format: 4 comma-separated fields:
+        - word 1 (pivot)
+        - word 2 (comparison)
+        - feature
+        - label ("1" if the feature characterizes word 1 compared to word 2, "0" otherwise)
+        Example:    word1,word2,feature,label(0/1)
+        """
+        linePos,lineNeg=0,0
+        for linea in file.readlines():
+            line=linea.split(",")
+            #print line
+            if line[3]=='1\n':          #agrega a positivos
+                linePos+=1
+                if diccPos.has_key((line[0],line[2])):
+                    diccPos[line[0],line[2]].append(line[1])
                 else:
-                    diccNeg[line[0],line[2]]=[line[1]]
-            else:
-                if not(diccPos.has_key((line[1],line[2]))):
-                    diccPos[line[1],line[2]]=[line[0]]
-    diccNeg=compareDic(diccPos,diccNeg)
-    print "Archivo: ",ruta
+                    diccPos[line[0],line[2]]=[line[1]]
+                if diccNeg.has_key((line[1],line[2])):
+                    diccNeg[line[1],line[2]].append(line[0])
+                else:
+                    diccNeg[line[1],line[2]]=[line[0]]
+            else:            #agrega a negativos
+                lineNeg+=1
+                if not(diccPos.has_key((line[0],line[2]))):
+                    if diccNeg.has_key((line[0],line[2])):
+                        diccNeg[line[0],line[2]].append(line[1])
+                    else:
+                        diccNeg[line[0],line[2]]=[line[1]]
+                else:
+                    if not(diccPos.has_key((line[1],line[2]))):
+                        diccPos[line[1],line[2]]=[line[0]]
+        diccNeg=compareDic(diccPos,diccNeg)
+        #print "Elementos dicPos ",len(diccPos),"\nelementos dicNeg ",len(diccNeg),"\n"
+        file.close()
     print "lineas Pos: ",linePos,"\nlineas Neg: ",lineNeg,"\n"
-    file.close()
     return diccPos,diccNeg
     
     
